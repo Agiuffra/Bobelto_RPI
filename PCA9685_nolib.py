b9e02fc5455b8fad2e697c66c8f27b1595e7bb31
Alessandro Giuffra <39315388+Agiuffra@users.noreply.github.com>
2022-05-22 01:14:03 -0500

    Update PCA9685_nolib.py

--- PCA9685_nolib.py
+++ PCA9685_nolib.py
@@ -12,21 +12,21 @@ MIN_PULSE = 900
 I2Cbus = smbus.SMBus(1)
 
 with smbus.SMBus(1) as I2Cbus:
-	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x00, 0x31)
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x00, [0x31])
 	time.sleep(0.001)
 	
 	prescalar = (25000000 / (4096 * FREQ * 0.92)) - 1
 	
-	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0xFE, byte(prescalar))
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0xFE, [byte(prescalar)])
 	time.sleep(0.001)
 	
-	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x00, 0xA1)
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x00, [0xA1])
 	time.sleep(0.001)
 	
-	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x01, 0x04)
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x01, [0x04])
 	time.sleep(0.001)
 	
-	pulse_us = 1500 # ms
+	pulse_us = 1500 # us
 	servoNum = 0
 
 	while(1):
