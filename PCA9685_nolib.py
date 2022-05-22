6e03a9a9627085a81866afa65b3a65cddc46e272
Alessandro Giuffra <39315388+Agiuffra@users.noreply.github.com>
2022-05-21 23:49:58 -0500

    Update PCA9685_nolib.py

--- PCA9685_nolib.py
+++ PCA9685_nolib.py
@@ -1,5 +1,5 @@
-from board import SCL, SDA
-import busio
+import sys
+import smbus2 as smbus
 import time
 
 PCA9685_ADDRESS = 0x7F
@@ -9,30 +9,35 @@ NEUTRAL_PULSE = 1500
 MAX_PULSE = 2100
 MIN_PULSE = 900
 
-i2c_bus = busio.I2C(SCL, SDA)
+I2Cbus = smbus.SMBus(1)
 
-i2c_bus.writeto(PCA9685_ADDRESS, [0x00, 0x31])
-time.sleep(0.001)
-
-prescalar = (25000000 / (4096 * FREQ * 0.92)) - 1
-
-i2c_bus.writeto(PCA9685_ADDRESS, [0xFE, byte(prescalar)])
-time.sleep(0.001)
-
-i2c_bus.writeto(PCA9685_ADDRESS, [0x00, 0xA1])
-time.sleep(0.001)
-
-i2c_bus.writeto(PCA9685_ADDRESS, [0x01, 0x04])
-time.sleep(0.001)
-
-pulse_us = 1500 # ms
-
-while(1):
-	period_us = 1000000/FREQ
-	count = (pulse_us/period_us) * 4096
-
-	LSB = byte(count)
-	MSB = byte(count) >> 8
-
-	i2c_bus.writeto(PCA9685_ADDRESS, [SERVO0 + (4 * servoNum), 0x00, 0x00, LSB, MSB])
+with smbus.SMBus(1) as I2Cbus:
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x00, 0x31)
 	time.sleep(0.001)
+	
+	prescalar = (25000000 / (4096 * FREQ * 0.92)) - 1
+	
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0xFE, byte(prescalar))
+	time.sleep(0.001)
+	
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x00, 0xA1)
+	time.sleep(0.001)
+	
+	I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, 0x01, 0x04)
+	time.sleep(0.001)
+	
+	pulse_us = 1500 # ms
+	servoNum = 0
+
+	while(1):
+		period_us = 1000000/FREQ
+		count = (pulse_us/period_us) * 4096
+
+		LSB = byte(count)
+		MSB = byte(count) >> 8
+
+		I2Cbus.write_i2c_block_data(PCA9685_ADDRESS, SERVO0 + (4 * servoNum), [0x00, 0x00, LSB, MSB])
+		time.sleep(0.001)
+		
+		
+		
